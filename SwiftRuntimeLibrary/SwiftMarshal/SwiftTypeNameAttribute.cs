cdbf217dc2909b7379c1fabf71579650bea65981
Stephen Hawley <stephen.hawley@xamarin.com>
2019-10-23 13:53:58 -0400

    White space, let's be consistent out there!

--- SwiftRuntimeLibrary/SwiftMarshal/SwiftTypeNameAttribute.cs
+++ SwiftRuntimeLibrary/SwiftMarshal/SwiftTypeNameAttribute.cs
@@ -1,7 +1,7 @@
-﻿using System;
-// Copyright (c) Microsoft Corporation.
+﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
 using System.Reflection;
 
 namespace SwiftRuntimeLibrary.SwiftMarshal {
