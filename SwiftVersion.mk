cba76e5bcce7c77f81ac2eda211012eaf1ef0eae
Stephen Hawley <stephen.hawley@xamarin.com>
2019-12-09 11:19:26 -0500

    Bumped hash

--- SwiftVersion.mk
+++ SwiftVersion.mk
@@ -2,4 +2,4 @@
 SWIFT_BRANCH=swift-5.0-branch-tomswifty
 SWIFT_SCHEME=swift-5.0-branch
 # this hash should be the most recent in the above branch
-SWIFT_HASH=ecf763a5b825fe076906e5e4f3050837f09a9ba1
+SWIFT_HASH=b6d821d3d70df4030fde1e0e2ed0f026d779e034
