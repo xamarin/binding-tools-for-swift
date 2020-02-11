11230f1941d29b9175b0f9de8ffd428add1ffa61
Stephen Hawley <stephen.hawley@xamarin.com>
2020-02-11 14:54:54 -0500

    This works. Time to walk away.

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -9,9 +9,8 @@ msbuild
 cd "$WORKSPACE/type-o-matic"
 msbuild
 cd "$WORKSPACE/swiftglue"
-#make generate-swift-bindings
+make generate-swift-bindings
 make all -j8
-nm "$WORKSPACE/swiftglue/bin/Debug/mac/FinalProduct/XamGlue.framework/XamGlue" | grep s7XamGlue12getSwiftType3str6resultSbSpySSG_SpyypXpGtF
 
 cd "$WORKSPACE"
 msbuild tom-swifty.sln
