d68d9d4e0b3eb038ae3d94b98395212da6c302a9
Chris Hamons <chris.hamons@xamarin.com>
2020-03-06 10:35:48 -0600

    [provisioning] Uninstalling cmake errors are 'fine', right? (#327)

--- jenkins/system-dependencies.sh
+++ jenkins/system-dependencies.sh
@@ -612,7 +612,8 @@ function install_cmake ()
 		git checkout "$BREW_CMAKE_HASH"
 		# Uninstall any existing cmakes. Ignore failures (which may happen if new cmake is installed)
 		if type -t cmake > /dev/null; then
-			brew uninstall --force cmake
+			brew uninstall --force cmake || true
+			brew unlink cmake || true
 		fi
 		# Install the cmake we want
 		HOMEBREW_NO_AUTO_UPDATE=1 brew install cmake
