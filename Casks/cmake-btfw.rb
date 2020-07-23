a648aaf83a62e58e21ee81b84af52d37fc9d4120
Manuel de la Pena <mandel@microsoft.com>
2020-07-23 15:37:29 -0400

    [DevOps] Add a tap for the project.
    Add a tap that contains different casks to be used with the project so
    that we do not need to do anything special regarding cmake and other
    deps.

---
+++ Casks/cmake-btfw.rb
@@ -0,0 +1,18 @@
+cask 'cmake-btfw' do
+  version '3.15.5'
+  sha256 '27ebcfd64c2b331db013906e283decd55a581163df3dd4546dd62a985b7df2cd'
+
+  url "https://www.cmake.org/files/v#{version.major_minor}/cmake-#{version}-Darwin-x86_64.dmg"
+  appcast 'https://cmake.org/files/LatestRelease/'
+  name 'CMake'
+  homepage 'https://cmake.org/'
+
+  conflicts_with formula: 'cmake'
+
+  app 'CMake.app'
+  binary "#{appdir}/CMake.app/Contents/bin/cmake"
+  binary "#{appdir}/CMake.app/Contents/bin/ccmake"
+  binary "#{appdir}/CMake.app/Contents/bin/cpack"
+  binary "#{appdir}/CMake.app/Contents/bin/ctest"
+  binary "#{appdir}/CMake.app/Contents/bin/cmake-gui"
+end
