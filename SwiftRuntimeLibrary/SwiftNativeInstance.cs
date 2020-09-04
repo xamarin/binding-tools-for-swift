ebe12592f71094312f09a686af3dca4027b1436a
Stephen Hawley <stephen.hawley@xamarin.com>
2020-09-04 10:14:22 -0400

    moving common code

---
+++ SwiftRuntimeLibrary/SwiftNativeInstance.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+
+namespace SwiftRuntimeLibrary {
+	public abstract class SwiftNativeInstance : IDisposable {
+		~SwiftNativeInstance ()
+		{
+			Dispose (false);
+		}
+		public void Dispose ()
+		{
+			Dispose (true);
+			GC.SuppressFinalize (this);
+		}
+		protected abstract void Dispose (bool disposing);
+	}
+}
