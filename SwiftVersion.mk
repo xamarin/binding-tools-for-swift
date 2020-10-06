469d366aa715e137de3d5a450636675e209e1833
Chris Hamons <chris.hamons@xamarin.com>
2020-10-06 16:00:18 -0500

    Start of build changes for xcode 12

--- SwiftVersion.mk
+++ SwiftVersion.mk
@@ -1,5 +1,5 @@
 # This file contains the information to determine exactly which swift version to use
-SWIFT_BRANCH=swift-5.0-branch-tomswifty
-SWIFT_SCHEME=swift-5.0-branch
+SWIFT_BRANCH=release/5.3-branch-tomswifty
+SWIFT_SCHEME=release/5.3
 # this hash should be the most recent in the above branch
-SWIFT_HASH=2169cbb1372ae9ca9d189da6ff2eaa41e00b814e
+SWIFT_HASH=9cfbe5490b3a710e85aecf15016836ee661d0382
