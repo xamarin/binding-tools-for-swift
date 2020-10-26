a4b61fe710cbf1658334524224503ba25cbdfdf3
TJ Lambert <tjlambert@microsoft.com>
2020-10-26 10:35:07 -0500

    new hash 5

--- SwiftVersion.mk
+++ SwiftVersion.mk
@@ -2,4 +2,4 @@
 SWIFT_BRANCH=release/5.3-TJ
 SWIFT_SCHEME=release/5.3-TJ
 # this hash should be the most recent in the above branch
-SWIFT_HASH=e55c6abe0b5884f30f35c49b8d13f921e9ecec29
+SWIFT_HASH=e49ec966c448ebac8699633e4de48fc858ddcc15
