470f31b80ac2f194746f08fd5c046638468ce1fd
TJ Lambert <tjlambert@microsoft.com>
2020-10-29 19:02:33 -0500

    adding in check for Xcode_12.app version

--- jenkins/build-swift.sh
+++ jenkins/build-swift.sh
@@ -119,6 +119,9 @@ git reset --hard "$SWIFT_HASH"
 echo "Updating swift dependencies..."
 ./utils/update-checkout --clone --skip-repository swift -j 1 --scheme "$SWIFT_SCHEME"
 
+echo "Checking version of Xcode_12.app"
+defaults read /Applications/Xcode_12.app/Contents/Info CFBundleShortVersionString
+
 echo "Building swift (not so swiftly, some patience is required)..."
 ./utils/build-script --clean -R --ios --tvos --watchos --extra-cmake-options=-DSWIFT_DARWIN_ENABLE_STABLE_ABI_BIT:BOOL=TRUE
 
