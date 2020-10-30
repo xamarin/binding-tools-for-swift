44db42e4bf3cee204d960a178a522a9d05ee5232
TJ Lambert <tjlambert@microsoft.com>
2020-10-30 15:13:47 -0500

    escaping the replacement string

--- jenkins/build-swift.sh
+++ jenkins/build-swift.sh
@@ -123,8 +123,8 @@ echo "Checking version of Xcode_12.app"
 defaults read /Applications/Xcode_12.app/Contents/Info CFBundleShortVersionString
 
 echo "Attempting to replace <math.h> in header file"
-NewMathHLocation='#include</Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/math.h>'
-sed -i '' -e "s/^<math.h>/${NewMathHLocation}/" /Applications/Xcode_12\.app/Contents/Developer/Toolchains/XcodeDefault\.xctoolchain/usr/include/c++/v1/cmath
+NewMathHLocation='#include<\/Applications\/Xcode\.app\/Contents\/Developer\/Toolchains\/XcodeDefault\.xctoolchain\/usr\/include\/c++\/v1\/math\.h>'
+sed -i '' -e "s/^<math.h>/${NewMathHLocation}/" /Applications/Xcode_12.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/cmath
 less -FX /Applications/Xcode_12.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/cmath
 
 echo "Building swift (not so swiftly, some patience is required)..."
