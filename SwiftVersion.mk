5cb4d4a59948f6c72a60c60e023a62530476d8f8
TJ Lambert <tjlambert@microsoft.com>
2020-11-03 09:39:59 -0600

    changing hash to get swift toolchain built on new path

--- SwiftVersion.mk
+++ SwiftVersion.mk
@@ -2,4 +2,4 @@
 SWIFT_BRANCH=release/5.3-TJ
 SWIFT_SCHEME=release/5.3-TJ
 # this hash should be the most recent in the above branch
-SWIFT_HASH=ae0b63ea25666b474345f143e85c3a7f5e182578
+SWIFT_HASH=0f3fddb19234c0286abf25f862e051804a9ecb0d
