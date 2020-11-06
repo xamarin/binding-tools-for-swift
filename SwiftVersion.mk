781dea4e84a42af44ce18f0044fb5253b8f1c969
TJ Lambert <tjlambert@microsoft.com>
2020-11-06 13:53:53 -0600

    cleaning and pointing to new BTfSR hash

--- SwiftVersion.mk
+++ SwiftVersion.mk
@@ -1,5 +1,5 @@
 # This file contains the information to determine exactly which swift version to use
-SWIFT_BRANCH=release/5.3-TJ
-SWIFT_SCHEME=release/5.3-TJ
+SWIFT_BRANCH=release/5.3-branch-tomswifty
+SWIFT_SCHEME=release/5.3
 # this hash should be the most recent in the above branch
-SWIFT_HASH=0f3fddb19234c0286abf25f862e051804a9ecb0d
+SWIFT_HASH=ac3b44a867f14d5a4f9b88d7d384d542646711f4
