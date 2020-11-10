caddf0542c387ffa81bf45134a3faf83c4586e0e
TJ Lambert <tjlambert@microsoft.com>
2020-11-10 16:09:38 -0600

    pointing SwiftVersion to my branch to test change recommended by Rolf - new one

--- SwiftVersion.mk
+++ SwiftVersion.mk
@@ -1,5 +1,5 @@
 # This file contains the information to determine exactly which swift version to use
-SWIFT_BRANCH=release/5.3-branch-tomswifty
+SWIFT_BRANCH=ClangIssue
 SWIFT_SCHEME=release/5.3
 # this hash should be the most recent in the above branch
-SWIFT_HASH=ac3b44a867f14d5a4f9b88d7d384d542646711f4
+SWIFT_HASH=bb46a02715273c2bcdfaf074304fdbf57eedb34c
