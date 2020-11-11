9881a78846a110007c71de370c1bf7b34e4a2e77
TJ Lambert <tjlambert@microsoft.com>
2020-11-11 09:51:13 -0600

    Implementing Rolf's suggestion in reflector

--- SwiftVersion.mk
+++ SwiftVersion.mk
@@ -1,5 +1,5 @@
 # This file contains the information to determine exactly which swift version to use
-SWIFT_BRANCH=ClangIssue
+SWIFT_BRANCH=release/5.3-branch-tomswifty
 SWIFT_SCHEME=release/5.3
 # this hash should be the most recent in the above branch
-SWIFT_HASH=bb46a02715273c2bcdfaf074304fdbf57eedb34c
+SWIFT_HASH=28e007252c2ec7217c7d75bd6f111b9c682153e3
