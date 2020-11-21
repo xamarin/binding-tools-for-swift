fd44b9b929d2a29eb45ce87ce4e39ce1c35e56f5
TJ Lambert <tjlambert@microsoft.com>
2020-11-21 09:51:26 -0600

    refactoring code

---
+++ ManualBinderFinder/StringBuilderExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ManualBinderFinder {
+	public static class StringBuilderExtension {
+		public static void RemoveDuplicateConsecutiveWords (this StringBuilder sb)
+		{
+			string pattern = @"\w*:";
+			bool isFinished = false;
+			while (!isFinished) {
+				MatchCollection matches = Regex.Matches (sb.ToString (), pattern);
+				if (matches.Count == 0) {
+					isFinished = true;
+					break;
+				}
+				for (var i = 0; i < matches.Count; i++) {
+					if (i < matches.Count - 1) {
+						if (matches [i].Value == matches [i + 1].Value) {
+							sb.Remove (matches [i + 1].Index, matches [i + 1].Length + 1);
+							break;
+						}
+					} else {
+						isFinished = true;
+						break;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ ManualBinderFinder/WriteXml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SwiftReflector.Inventory;
+
+namespace ManualBinderFinder {
+	public class WriteXml {
+		public static void CreateXmlFile (string moduleName, ModuleInventory mi)
+		{
+			List<ClassContents> classesList = CheckInventory.GetClasses (mi);
+			List<ClassContents> enumsList = CheckInventory.GetEnums (mi);
+			List<ClassContents> structsList = CheckInventory.GetStructs (mi);
+			List<ProtocolContents> protocolList = CheckInventory.GetProtocols (mi);
+
+			WriteXmlFile (moduleName, classesList, enumsList, structsList, protocolList);
+
+		}
+
+		static void WriteXmlFile (string moduleName, List<ClassContents> classesList, List<ClassContents> enumsList, List<ClassContents> structsList, List<ProtocolContents> protocolsList)
+		{
+			//using (StreamWriter sw = new StreamWriter ($"../../../Modules/{moduleName}.xml")) {
+			var version = "1.011";
+			using (StreamWriter sw = new StreamWriter ($"./Modules/{moduleName}.xml")) {
+				Console.WriteLine ($"Creating xml output for {moduleName} at ./Modules/{moduleName}.xml");
+
+				sw.WriteXmlIntro (moduleName, version);
+
+				sw.WriteClasses (moduleName, classesList);
+
+				sw.WriteStructs (moduleName, structsList);
+
+				sw.WriteEnums (moduleName, enumsList);
+
+				sw.WriteProtocols (moduleName, protocolsList);
+
+				sw.WriteXmlOutro ();
+
+
+			}
+		}
+	}
+}
