75a78c59a523160b40d9b56610366c1e172451d2
TJ Lambert <tjlambert@microsoft.com>
2020-11-21 11:17:42 -0600

    organizing the way to search libraries and changing the way it searches

--- ManualBinderFinder/Program.cs
+++ ManualBinderFinder/Program.cs
@@ -20,10 +20,10 @@ namespace ManualBinderFinder {
 				return;
 			}
 
-			if (options.dylibLibraryList.Count == 0){
-				Console.WriteLine ("library is empty. Try --library=<dylib name>");
-				return;
-			}
+			//if (options.dylibLibraryList.Count == 0){
+			//	Console.WriteLine ("library is empty. Try --library=<dylib name>");
+			//	return;
+			//}
 
 			if (string.IsNullOrEmpty (options.platform)) {
 				options.platform = "all";
@@ -42,27 +42,58 @@ namespace ManualBinderFinder {
 			Console.WriteLine ($"{options.platform}");
 			Console.WriteLine ($"{options.architecture}");
 
-			foreach (string lib in options.dylibLibraryList) {
-				string libName = lib;
-				if (!libName.Contains (".dylib")) {
-					libName += ".dylib";
+			string bashString = BuildBashString ("all", options.platform, options.architecture);
+			Console.WriteLine (bashString);
+			var libraries = GetLibraries (bashString);
+
+			foreach (var lib in libraries) {
+				try {
+					var errors = new ErrorHandling ();
+					var mi = ModuleInventory.FromFile (lib.Value, errors);
+					WriteXml.CreateXmlFile (lib.Key, mi);
+				} catch (Exception e) {
+					Console.WriteLine ($"Could not create xml for {lib.Key} - {lib.Value}. {e.Message}");
 				}
-				var shellOutput = Shell.RunBash ($"find ../SwiftToolchain*/ -iname {libName}");
-				//var shellOutput = Shell.RunBash ($"find ../../../../SwiftToolchain*/ -iname {libName}");
-
-				if (!shellOutput.Contains (".dylib")) {
-					Console.WriteLine ($"{libName} was not valid");
-					return;
-				}
-				string dylibPath = shellOutput.Substring (0, shellOutput.IndexOf (Environment.NewLine));
-				string dylibName = dylibPath.Split ('/').Last ().Split ('.').First ();
-				var errors = new ErrorHandling ();
-
-				var mi = ModuleInventory.FromFile (dylibPath, errors);
-
-				WriteXml.CreateXmlFile (dylibName, mi);
 			}
-			
+		}
+
+		static string BuildBashString (string name, string platform, string architecture)
+		{
+			string n;
+			string p;
+			if (name == "all")
+				n = "*.dylib";
+			else
+				n = name;
+
+			if (platform == "all")
+				p = "*";
+			else
+				p = platform;
+
+			if (architecture == "all") {
+				return $"find ../SwiftToolchain*/build/Ninja-ReleaseAssert/swift-macosx-x86_64/lib/swift/{p}/ -type f -iname \"{n}\"";
+				//return $"find ../../../../SwiftToolchain*/build/Ninja-ReleaseAssert/swift-macosx-x86_64/lib/swift/{p}/ -type f -iname \"{n}\"";
+			} else {
+				return $"find ../SwiftToolchain*/build/Ninja-ReleaseAssert/swift-macosx-x86_64/lib/swift/{p}/{architecture}/ -type f -iname \"{n}\"";
+				//return $"find ../../../../SwiftToolchain*/build/Ninja-ReleaseAssert/swift-macosx-x86_64/lib/swift/{p}/{architecture}/ -type f -iname \"{n}\"";
+			}
+		}
+
+		static Dictionary<string, string> GetLibraries (string bashString)
+		{
+			Dictionary<string, string> libraries = new Dictionary<string, string> ();
+			var shellOutput = Shell.RunBash (bashString);
+			var libraryPaths = shellOutput.Split (Environment.NewLine);
+
+			foreach (var lib in libraryPaths) {
+				// Question for Steve, are the dylibs different for different architectures & platforms
+				// I think probably, but here I am ignoring them
+				var libName = lib.Split ('/').Last ().Split ('.').First ();
+				if (!libraries.ContainsKey (libName))
+					libraries.Add (libName, lib);
+			}
+			return libraries;
 		}
 	}
 }
