56db8fe1f90e75893c524d9449781e8098f8ffa2
TJ Lambert <tjlambert@microsoft.com>
2021-04-05 18:06:46 -0500

    changes to DBGenericParameters to use extension methods in the constructors

---
+++ DylibBinder/HashSetExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DylibBinder {
+	internal static class HashSetExtensions {
+		public static void AddRange<T> (this HashSet<T> t, params IEnumerable<T> [] entriesArray)
+		{
+			foreach (var entries in entriesArray) {
+				foreach (var entry in entries) {
+					t.Add (entry);
+				}
+			}
+		}
+	}
+}
