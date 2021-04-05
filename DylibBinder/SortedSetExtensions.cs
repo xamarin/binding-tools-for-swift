759f7b02856c685d0ad1260c4cc9cf71e2abbd24
TJ Lambert <tjlambert@microsoft.com>
2021-04-05 13:23:37 -0500

    adding in ignore list

--- DylibBinder/SortedSetExtensions.cs
+++ DylibBinder/SortedSetExtensions.cs
@@ -5,7 +5,7 @@ using SwiftReflector.Inventory;
 
 namespace DylibBinder {
 	internal static class SortedSetExtensions {
-		internal static void AddRange<T> (this SortedSet<T> t, params IEnumerable<T> [] entriesArray)
+		public static void AddRange<T> (this SortedSet<T> t, params IEnumerable<T> [] entriesArray)
 		{
 			foreach (var entries in entriesArray) {
 				foreach (var entry in entries) {
@@ -14,34 +14,40 @@ namespace DylibBinder {
 			}
 		}
 
-		internal static SortedSet<OverloadInventory> CreateOverloadSortedSet ()
+		public static SortedSet<OverloadInventory> CreateOverloadSortedSet ()
 		{
 			Comparison<OverloadInventory> comp = (x, y) => string.Compare (x.Name.Name, y.Name.Name);
 			return new SortedSet<OverloadInventory> (Comparer<OverloadInventory>.Create (comp));
 		}
 
-		internal static SortedSet<TLFunction> CreateTLFunctionSortedSet ()
+		public static SortedSet<TLFunction> CreateTLFunctionSortedSet ()
 		{
 			Comparison<TLFunction> comp = (x, y) => string.Compare (x.Module.Name, y.Module.Name);
 			return new SortedSet<TLFunction> (Comparer<TLFunction>.Create (comp));
 		}
 
-		internal static SortedSet<PropertyContents> CreatePropertySortedSet ()
+		public static SortedSet<PropertyContents> CreatePropertySortedSet ()
 		{
 			Comparison<PropertyContents> comp = (x, y) => string.Compare (x.Name.Name, y.Name.Name);
 			return new SortedSet<PropertyContents> (Comparer<PropertyContents>.Create (comp));
 		}
 
-		internal static SortedSet<ProtocolContents> CreateProtocolSortedSet ()
+		public static SortedSet<ProtocolContents> CreateProtocolSortedSet ()
 		{
 			Comparison<ProtocolContents> comp = (x, y) => string.Compare (x.Name.ToFullyQualifiedName (), y.Name.ToFullyQualifiedName ());
 			return new SortedSet<ProtocolContents> (Comparer<ProtocolContents>.Create (comp));
 		}
 
-		internal static SortedSet<ClassContents> CreateClassSortedSet ()
+		public static SortedSet<ClassContents> CreateClassSortedSet ()
 		{
 			Comparison<ClassContents> comp = (x, y) => string.Compare (x.Name.ToFullyQualifiedName (), y.Name.ToFullyQualifiedName ());
 			return new SortedSet<ClassContents> (Comparer<ClassContents>.Create (comp));
+		}
+
+		public static SortedSet<string> CreateStringSortedSet ()
+		{
+			Comparison<string> comp = (x, y) => string.Compare (x, y);
+			return new SortedSet<string> (Comparer<string>.Create (comp));
 		}
 	}
 }
